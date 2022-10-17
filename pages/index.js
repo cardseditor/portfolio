48a7113660ccd4e59b32165b81a620367467e3de
cardseditor <toshiki.u314@gmail.com>
2022-10-18 03:26:31 +0900

    chore: add icon

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,23 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../componets/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../componets/date'
+import Head from "next/head";
+import Layout, { siteTitle } from "../componets/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../componets/date";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons";
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = getSortedPostsData();
   return {
     props: {
-      allPostsData
-    }
-  }
+      allPostsData,
+    },
+  };
 }
 
 export default function Home({ allPostsData }) {
@@ -21,20 +27,76 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p className={utilStyles.selfIntro}>I’m CardsEditor!</p>
+        <div className={utilStyles.selfIntro}>I’m CardsEditor!</div>
 
-        <p className={utilStyles.selfIntro}>
-          <a href='https://twitter.com/CardsEditor' target="_blank">Twitter</a>
-        </p>
-        <p className={utilStyles.selfIntro}>
-          <a href='https://www.instagram.com/cardseditor/' target="_blank">Instagram</a>
-        </p>
-        <p className={utilStyles.selfIntro}>
-          <a href='https://github.com/cardseditor' target="_blank">GitHub</a>
-        </p>
-        <p className={utilStyles.selfIntro}>
-          <a href='https://www.youtube.com/channel/UCD4Vokjng44fu0vBpY50odg' target="_blank">YouTube</a>
-        </p>
+        <a href="https://twitter.com/cardseditor" target="_blank">
+          <div className={utilStyles.selfIntro}>
+            <FontAwesomeIcon
+              icon={faTwitter}
+              style={{
+                width: "24px",
+                color: "gray",
+                marginRight: "20px",
+              }}
+            />
+            <div>Twitter</div>
+          </div>
+        </a>
+        <a href="https://www.instagram.com/cardseditor/" target="_blank">
+          <div className={utilStyles.selfIntro}>
+            <FontAwesomeIcon
+              icon={faInstagram}
+              style={{
+                width: "24px",
+                color: "gray",
+                marginRight: "20px",
+              }}
+            />
+            <div>Instagram</div>
+          </div>
+        </a>
+        <a href="https://gajeroll.com/" target="_blank">
+          <div className={utilStyles.selfIntro}>
+            <FontAwesomeIcon
+              icon={faPen}
+              style={{
+                width: "24px",
+                color: "gray",
+                marginRight: "20px",
+              }}
+            />
+            <div>Blog (Gajeroll)</div>
+          </div>
+        </a>
+        <a href="https://github.com/cardseditor" target="_blank">
+          <div className={utilStyles.selfIntro}>
+            <FontAwesomeIcon
+              icon={faGithub}
+              style={{
+                width: "24px",
+                color: "gray",
+                marginRight: "20px",
+              }}
+            />
+            <div>GitHub</div>
+          </div>
+        </a>
+        <a
+          href="https://www.youtube.com/channel/UCD4Vokjng44fu0vBpY50odg"
+          target="_blank"
+        >
+          <div className={utilStyles.selfIntro}>
+            <FontAwesomeIcon
+              icon={faYoutube}
+              style={{
+                width: "24px",
+                color: "gray",
+                marginRight: "20px",
+              }}
+            />
+            <div>YouTube</div>
+          </div>
+        </a>
       </section>
       {/* <section className={utilStyles.headingMd}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
@@ -51,6 +113,7 @@ export default function Home({ allPostsData }) {
             </li>          ))}
         </ul>
       </section> */}
+      <style jsx>{``}</style>
     </Layout>
-  )
+  );
 }
