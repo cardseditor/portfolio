44829efbebc08617b424c13cfcd2a96f31949ecc
cardseditor <toshiki.u314@gmail.com>
2023-07-15 00:31:08 +0900

    feat: 成果を追加

--- componets/date.js
+++ componets/date.js
@@ -2,5 +2,5 @@ import { parseISO, format } from 'date-fns'
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'yyyy. LL. dd')}</time>
+  return <time dateTime={dateString}>{format(date, 'yyyy.LL.dd')}</time>
 }
